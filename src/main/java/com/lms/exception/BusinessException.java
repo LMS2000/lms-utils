62fb7e32001a964aaacf8df7d4c74c79ed69a19f
LMS <956287248@qq.com>
2024-03-21 13:28:55 +0800

    添加基础的公共类，还有本地缓存

---
+++ src/main/java/com/lms/exception/BusinessException.java
@@ -0,0 +1,68 @@
+package com.lms.exception;
+
+
+import com.lms.contants.HttpCode;
+
+/**
+ * 自定义异常类
+ * @author lms2000
+ */
+public class BusinessException extends RuntimeException {
+
+    private final int code;
+
+    public BusinessException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BusinessException(HttpCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+
+    public BusinessException(HttpCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+    }
+
+    public static void throwIfOperationAdmin(Integer uid) {
+        throwIf(uid == 1);
+    }
+
+    public static void throwIf(boolean flag) {
+        if (flag) throw new BusinessException(HttpCode.OPERATION_ERROR);
+    }
+
+    public static void throwIfNot(boolean flag) {
+        if (!flag) throw new BusinessException(HttpCode.OPERATION_ERROR);
+    }
+
+    public static void throwIf(boolean flag, HttpCode httpCode) {
+        if (flag) throw new BusinessException(httpCode);
+    }
+
+    public static void throwIf(boolean flag, String message) {
+        if (flag) throw new BusinessException(HttpCode.PARAMS_ERROR, message);
+    }
+
+    public static void throwIfNot(boolean flag, String message) {
+        if (flag) throw new BusinessException(HttpCode.PARAMS_ERROR, message);
+    }
+
+    public static void throwIfNot(boolean flag, HttpCode httpCode) {
+        if (!flag) throw new BusinessException(httpCode);
+    }
+
+    public static void throwIf(boolean flag, HttpCode httpCode, String message) {
+        if (flag) throw new BusinessException(httpCode, message);
+    }
+
+    public static void throwIfNot(boolean flag, HttpCode httpCode, String message) {
+        if (!flag) throw new BusinessException(httpCode, message);
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
